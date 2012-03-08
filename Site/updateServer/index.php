74d4afdd400261d4c477aa1f47223a2e4a92c8a1
Matthew Barbour <sanmadjack@masgau.org>
2012-03-08 15:55:41 -0600

    Added links to the update php to help quickly test the generation of xml data files

--- Site/updateServer/index.php
+++ Site/updateServer/index.php
@@ -1,24 +1,26 @@
 <?php
+
 function curPageURL() {
- $pageURL = 'http';
- $pageURL .= "://";
- if ($_SERVER["SERVER_PORT"] != "80") {
-  $pageURL .= $_SERVER["SERVER_NAME"].":".$_SERVER["SERVER_PORT"].$_SERVER["REQUEST_URI"];
- } else {
-  $pageURL .= $_SERVER["SERVER_NAME"].$_SERVER["REQUEST_URI"];
- }
- return $pageURL;
+    $pageURL = 'http';
+    $pageURL .= "://";
+    if ($_SERVER["SERVER_PORT"] != "80") {
+        $pageURL .= $_SERVER["SERVER_NAME"] . ":" . $_SERVER["SERVER_PORT"] . $_SERVER["REQUEST_URI"];
+    } else {
+        $pageURL .= $_SERVER["SERVER_NAME"] . $_SERVER["REQUEST_URI"];
+    }
+    return $pageURL;
 }
 
+ini_set('default_charset', 'UTF-8');
+define('MASGAU', 'whut');
+require_once '../DBSettings.php';
+$link = mysql_connect($wgDBserver, $wgDBuser, $wgDBpassword);
+mysql_set_charset('utf8', $link);
+
 if (isset($_GET['version'])) {
-    ini_set('default_charset', 'UTF-8');
     header("Content-Type:text/xml; charset=UTF-8'");
-    define('MASGAU', 'whut');
 
 
-    require_once '../DBSettings.php';
-    $link = mysql_connect($wgDBserver, $wgDBuser, $wgDBpassword);
-    mysql_set_charset('utf8',$link);
 
 
     if (isset($_GET['file'])) {
@@ -67,13 +69,30 @@ if (isset($_GET['version'])) {
                     appendChild($xml->createTextNode(
                                     MASGAUBase::formatDate($row['last_updated'])));
             $file->appendChild($xml->createAttribute("url"))->
-                    appendChild($xml->createTextNode(curPageURL().'&file='.$row['name']));
+                    appendChild($xml->createTextNode(curPageURL() . '&file=' . $row['name']));
         }
 
         $output = $xml->saveXML();
         echo $output;
     }
 } else {
-    echo "VERSION NOT PROVIDED";
+    echo "VERSION NOT PROVIDED, RUNNING IN TEST MODE<br />";
+
+    $sql = 'select * from masgau_game_data.xml_files order by name asc';
+    $sql_ver = 'select * from masgau_game_data.xml_versions order by string asc';
+
+    $result = mysql_query($sql);
+    $result_ver = mysql_query($sql_ver);
+    echo "<ul>";
+    while ($row_ver = mysql_fetch_assoc($result_ver)) {
+        echo "<li><a href='?version=" . $row_ver['string'] . "'>" . $row_ver['string'] . "</a></li>";
+        echo "<ul>";
+        while ($row = mysql_fetch_assoc($result)) {
+            echo "<li><a href='?version=" . $row_ver['string'] . "&file=" . $row['name'] . "'>" . $row['name'] . "</li>";
+        }
+        echo "</ul>";
+        mysql_data_seek($result, 0);
+    }
+    echo "</ul>";
 }
 ?>
